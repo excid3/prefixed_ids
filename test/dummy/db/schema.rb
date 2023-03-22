3741c3d38bfea4da0908f7511d9883aee94e7f47
Chris Oliver <excid3@gmail.com>
2023-03-22 09:23:15 -0500

    Update schema version for compatibility

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_22_131821) do
+ActiveRecord::Schema[6.0].define(version: 2023_03_22_131821) do
   create_table "accounts", force: :cascade do |t|
     t.integer "user_id"
     t.datetime "created_at", null: false
