e3053e8cc0c1f82f61cbe0b76c657fa5e1bb3635
CJ Avilla <cjavilla@gmail.com>
2024-11-13 14:27:56 -0500

    Faster way to access prefix IDs for associated models

--- test/prefixed_ids_test.rb
+++ test/prefixed_ids_test.rb
@@ -24,7 +24,7 @@ class PrefixedIdsTest < ActiveSupport::TestCase
   test "can get prefix IDs from multiple original IDs" do
     assert_equal(
       [users(:one).prefix_id, users(:two).prefix_id, users(:three).prefix_id],
-      User.prefix_ids([users(:one).id, users(:two).id, users(:three).id])
+      User.prefix_ids([users(:one).id, users(:two).id, users(:three).id]),
     )
   end
 
@@ -35,7 +35,7 @@ class PrefixedIdsTest < ActiveSupport::TestCase
   test "can get original IDs from multiple prefix IDs" do
     assert_equal(
       [users(:one).id, users(:two).id, users(:three).id],
-      User.decode_prefix_ids([users(:one).prefix_id, users(:two).prefix_id, users(:three).prefix_id])
+      User.decode_prefix_ids([users(:one).prefix_id, users(:two).prefix_id, users(:three).prefix_id]),
     )
   end
 
@@ -201,6 +201,28 @@ class PrefixedIdsTest < ActiveSupport::TestCase
 
   test "calling to_param on non-persisted record" do
     assert_nil Post.new.to_param
+  end
+
+  test "helper for getting prefix ID from belongs to models without loading the record" do
+    post = posts(:one)
+    assert_equal post.user_prefix_id, post.user.prefix_id
+  end
+
+  test "setter for using prefix ID while creating models with mass assignment" do
+    post = Post.create!(user_prefix_id: users(:two).prefix_id)
+    assert_equal users(:two), post.user
+  end
+
+  test "setter for belongs to models" do
+    post = Post.new
+    post.user_prefix_id = users(:two).prefix_id
+    assert_equal users(:two), post.user
+  end
+
+  test "setter not created on models without has_prefix_id" do
+    assert_raises NoMethodError do
+      NonprefixedItem.new.user_prefix_id
+    end
   end
 
   if PrefixedIds::Test.rails71_and_up?
